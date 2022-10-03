b5bcbb591fd5175cda9ec477368ae0d55af774cc
Simeon Georgiev <sgeorgiev783@gmail.com>
2022-10-03 19:51:40 +0300

    added navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../assets/images/logo.png'
+import styles from './Navbar.module.css'
+
+const Navbar = () => {
+  return (
+    <header className={styles['main-header']}>
+        <div className={styles['logo-container']}>
+            <Link className="home" to="/">
+                <img src={logo} alt='logo'/>
+            </Link>
+        </div>
+        <nav>
+            <Link to="/catalog">All books</Link>
+
+            <div className={styles.links}>
+                <Link to="/create">Add Books</Link>
+                <Link to="/create">My Profile</Link>
+                <Link to="/logout">Logout</Link>
+            </div>
+
+            <div className={styles.links}>
+                <Link to="/login">Login</Link>
+                <Link to="/register">Register</Link>
+            </div>
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
